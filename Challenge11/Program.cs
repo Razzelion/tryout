fcd8d8ae1888adf5fabfb1f7744c70264debd092
Razzelion <38034003+Razzelion@users.noreply.github.com>
2019-11-11 18:21:44 +0700

    Update Program.cs

--- Challenge11/Program.cs
+++ Challenge11/Program.cs
@@ -1,14 +1,45 @@
-﻿using System;
+using System.Linq;  
+using System;
 
-namespace SumArgument
+namespace SumArguments
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // Sum all args
-            // You can run "dotnet run -- 1 5 8 3"
-            // The output should be 17
+            int i;  
+            int[] a = new int[30];
+            Console.Write("jumlah angka : ");  
+            
+            int n = Convert.ToInt16(Console.ReadLine());   
+            
+            for (i = 1; i <= n; i++)  
+            {  
+            Console.Write("masukkan angka ke " + i + ":");  
+            
+            a[i] = Convert.ToInt16(Console.ReadLine());   
+            }  
+            
+            for (i = 1; i <= n; i++)  
+            {  
+            for (int j = 1; j <= n - 1; j++)  
+            {  
+                if (a[j] > a[j + 1])  
+                {  
+                    int temp = a[j];  
+                    a[j] = a[j + 1];  
+                    a[j + 1] = temp;  
+                }  
+            }  
+            }  
+       
+        Console.Write("Jumlah : "); 
+        int sum = 0; 
+        for (i = 1; i <= n; i++)  
+        {  
+            sum += a[i];
+        }
+        Console.Write($"{sum} ");
         }
     }
 }
