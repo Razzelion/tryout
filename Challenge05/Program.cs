ca9aa24575ff80b6891c4e2e347da71d1dd0ba83
Razzelion <38034003+Razzelion@users.noreply.github.com>
2019-11-11 19:54:17 +0700

    Update Program.cs

--- Challenge05/Program.cs
+++ Challenge05/Program.cs
@@ -8,8 +8,43 @@ namespace EvenOdd
         {
             // Create new method with ability to detect odd or even number.
             // Example:
-            // IsOdd(4) // false
-            // IsEven(8) // true
+            IsOdd(4); // false
+            IsEven(8); // true
+            IsOdd(18);
+            IsOdd(13);
+            IsEven(244);
+            IsEven(10001);
+            //int no1;
+            
+
+            //Console.Write("Masukkan angka : ");
+            //no1 = Convert.ToInt32(Console.ReadLine());
+
+            
+
+
+            
+        }
+
+        private static double IsOdd(double value)
+        {
+            Console.Write($"IsOdd {value} ? ");
+            if(value % 2 == 0)
+            Console.Write("FALSE \n");
+            else
+            Console.Write("TRUE \n");
+            return 0;
+        }
+
+        private static double IsEven(double value)
+        {
+            Console.Write($"IsEven {value} ? ");
+            if(value % 2 == 0)
+            Console.Write("TRUE \n");
+            else
+            Console.Write("FALSE \n");
+            return 0;
         }
     }
 }
+
