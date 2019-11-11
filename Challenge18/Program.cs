a5aef9e56d28b82360fd7e614d4140703ff7f300
Razzelion <38034003+Razzelion@users.noreply.github.com>
2019-11-11 15:16:55 +0700

    Update Program.cs

--- Challenge18/Program.cs
+++ Challenge18/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Linq;
 
 namespace FindMinMaxAvg
 {
@@ -11,20 +12,29 @@ namespace FindMinMaxAvg
             FindMax(numbers); // Output: 330
             FindMin(numbers); // Output: -65
             FindAverage(numbers); // Output: 52.16
+
+            
         }
 
         static int FindMax(int[] numbers)
         {
+
+            int m = numbers.Max();
+            Console.WriteLine(m);
             return 0;
         }
 
         static int FindMin(int[] numbers)
         {
+            int m = numbers.Min();
+            Console.WriteLine(m);
             return 0;
         }
 
         static int FindAverage(int[] numbers)
         {
+            double m = numbers.Average();
+            Console.WriteLine(m);
             return 0;
         }
     }
