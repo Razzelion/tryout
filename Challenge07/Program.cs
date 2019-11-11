5b2bd8b3da44fc110376c8ddab501763f783d9f8
Razzelion <38034003+Razzelion@users.noreply.github.com>
2019-11-11 19:08:17 +0700

    Update Program.cs

--- Challenge07/Program.cs
+++ Challenge07/Program.cs
@@ -17,31 +17,55 @@ namespace TempratureConverter
 
         private static double CelciusToFahrenheit(double value)
         {
+            double fahrenheit;        
+            Console.WriteLine("Celsius: " + value);
+            fahrenheit = (value * 9) / 5 + 32;
+            Console.WriteLine("Fahrenheit: " + fahrenheit + "\n");
             return 0;
         }
 
         private static double FahrenheitToCelcius(double value)
         {
+            double celcius;        
+            Console.WriteLine("Fahrenheit: " + value);
+            celcius = (value-32) * 5/9;
+            Console.WriteLine("Celcius: " + celcius + "\n");
             return 0;
         }
 
         private static double CelciusToKelvin(double value)
         {
+            double kelvin;        
+            Console.WriteLine("Celsius: " + value);
+            kelvin = (value + 273.15);
+            Console.WriteLine("Kelvin: " + kelvin + "\n");
             return 0;
         }
 
         private static double KelvinToCelcius(double value)
         {
+            double celcius;        
+            Console.WriteLine("Kelvin: " + value);
+            celcius = (value - 273.15);
+            Console.WriteLine("Celcius: " + celcius + "\n");
             return 0;
         }
 
         private static double KelvinToFahrenheit(double value)
         {
+            double fahrenheit;        
+            Console.WriteLine("Kelvin: " + value);
+            fahrenheit = (value - 273.15)* 9/5 + 32;
+            Console.WriteLine("fahrenheit: " + fahrenheit + "\n");
             return 0;
         }
 
         private static double FahrenheitToKelvin(double value)
         {
+            double kelvin;        
+            Console.WriteLine("Fahrenheit: " + value);
+            kelvin = ((value-32) * 5/9)+273.15;
+            Console.WriteLine("Kelvin: " + kelvin + "\n");
             return 0;
         }
     }
