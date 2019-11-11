ab0fbdb1764003129baa5903b0ff690841c766bc
Razzelion <38034003+Razzelion@users.noreply.github.com>
2019-11-11 20:11:05 +0700

    Update Program.cs

--- Challenge02/Program.cs
+++ Challenge02/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Linq;
 
 namespace DaysToNextBirthday
 {
@@ -6,9 +7,27 @@ namespace DaysToNextBirthday
     {
         static void Main(string[] args)
         {
-            var today = DateTime.Now;
-            // Count the days of your next birthday
-            Console.WriteLine("{0} days remaining to my next birthday.");
+            //var today = DateTime.Today;
+            // Count the days of your next Ultah
+              DateTime Ultah = new DateTime(1988, 05, 23);
+              DateTime now = DateTime.Today;
+              int months = 0;
+              int days = 0;
+
+              DateTime nextUltah = Ultah.AddYears(now.Year - Ultah.Year);
+              if (nextUltah < now)
+              {
+                  nextUltah = nextUltah.AddYears(1);
+              }
+
+              while (now.AddMonths(months + 1) <= nextUltah)
+              {
+                  months++;
+              }
+              days = nextUltah.Subtract(now.AddMonths(months)).Days;
+
+              Console.WriteLine("{0} month(s) and {1} day(s) remaining to my next birthday.", months, days);
+            
         }
     }
 }
