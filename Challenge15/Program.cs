5ac89d6a08052a820e408b2e93ec1f41e384b973
Razzelion <38034003+Razzelion@users.noreply.github.com>
2019-11-11 15:50:23 +0700

    Update Program.cs

--- Challenge15/Program.cs
+++ Challenge15/Program.cs
@@ -1,17 +1,30 @@
-﻿using System;
-
+using System;
+using System.Linq;
+using System.Collections.Generic;
 namespace MirrorString
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Mirror("--vv"); // Output: --vvvv--
+            //Mirror("--vv"); // Output: --vvvv--
+
+            char[] s = {'-','-','v','v'};
+            Console.Write("String = ");
+            foreach(char arr in s) {
+            Console.Write(arr);
+            }
+
+            IQueryable<char> res = s.AsQueryable().Reverse(); 
+            Console.Write("\nMirror = ");
+            Console.Write(s);
+            foreach (char c in res) {
+            Console.Write(c); 
+            }
         }
 
-        static string Mirror(string text)
-        {
-            return "";
-        }
+
+
+        
     }
 }
