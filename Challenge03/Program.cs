a4af174afac25c81fa293b63b37d42f5bb4609a4
Razzelion <38034003+Razzelion@users.noreply.github.com>
2019-11-11 20:21:27 +0700

    Update Program.cs

--- Challenge03/Program.cs
+++ Challenge03/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace StringLength
 {
@@ -10,8 +10,37 @@ namespace StringLength
             // That method should accept a string and will
             // return length of characters input.
 
-            Console.WriteLine("'Hello World!' have {0} characters", CountString("Hello World!"));
-            Console.WriteLine("'Hello Universe!' have {0} characters", CountString("Hello Universe!"));
-        }
+            //Console.WriteLine("'Hello World!' have {0} characters", CountString("Hello World!"));
+            //Console.WriteLine("'Hello Universe!' have {0} characters", CountString("Hello Universe!"));
+
+            string str;
+            int alp, digit, splch, i,l;
+            alp = i = 0;
+
+            Console.Write("Input the string : ");
+            str = Console.ReadLine();	
+            l=str.Length; 
+
+            while(i<l)
+            {
+            if((str[i]>='a' && str[i]<='z') || (str[i]>='A' && str[i]<='Z'))
+            {
+                alp++;
+            }
+            else if(str[i]>='0' && str[i]<='9')
+            {
+                alp++;
+            }
+            else
+            {
+                alp++;
+            }
+
+            i++;
+
+            
+            }
+            Console.WriteLine($"{str} have {alp} characters");
+        }    
     }
 }
