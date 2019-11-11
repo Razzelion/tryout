2084cf10c05333633dc06c84f386298fcab1e28e
Razzelion <38034003+Razzelion@users.noreply.github.com>
2019-11-11 13:23:24 +0700

    Update Program.cs

--- Challenge01/Program.cs
+++ Challenge01/Program.cs
@@ -6,11 +6,9 @@ namespace YourName
     {
         static void Main(string[] args)
         {
-            // Get your name by using Console.ReadLine()
-            // Example output: "Hello, my name is Salamander.",
-            // depends on user input.
-
-            Console.WriteLine("Hello, my name is {0}.");
+            Console.WriteLine("input name:");
+            string input = Console.ReadLine();
+            Console.WriteLine("Hello, my name is {input}.");
         }
     }
 }
